a70d1f710a70a09019c3f41555b93216f804fb7b
Virginia Corvid <vcorvid@tableau.com>
2017-08-11 17:19:33 -0700

    renamed and rejiggered coveoWDC files Hello WDC works

--- Examples/js/coveoWDC.js
+++ Examples/js/coveoWDC.js
@@ -2,19 +2,22 @@
     var myConnector = tableau.makeConnector();
 
     myConnector.getSchema = function(schemaCallback) {
-
+        tableau.log("Hello WDC!");
     };
+
+
 
     myConnector.getData = function(table, doneCallback) {
 
     };
 
     tableau.registerConnector(myConnector);
-    
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed";
+            tableau.connectionName = "Coveo Usage Analytics";
             tableau.submit();
         });
     });
+
+
 })();
